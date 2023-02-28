0aa86f59d524d4a2bddbe92ba18e33712fd201d7
Park JW <p.jangwoo@gmail>
2023-02-28 19:44:21 +0900

    2월 28일 완료

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
 
+	//select * from item_img where item_id = ? order by id asc  
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
     ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn);
